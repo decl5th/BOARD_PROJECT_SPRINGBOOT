44a125768ae76237cf9f4430310e24f6ab6ff919
decl5th <alsgur6624@gmail.com>
2024-07-26 15:51:55 +0900

    메세지 관련 설정 및 시큐리티 회원 비번 관련 인코딩 설정

--- src/main/java/org/choongang/global/configs/MvcConfig.java
+++ src/main/java/org/choongang/global/configs/MvcConfig.java
@@ -1,8 +1,22 @@
 package org.choongang.global.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableJpaAuditing
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    /**
+     * <input type="hidden" name="_method" value="patch"> -> patch 방식으로 요청
+     * ?_method=DELETE
+     * @return
+     */
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+        return new HiddenHttpMethodFilter();
+    }
 }
