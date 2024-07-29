7e2122f894114a323709b53bba7b46cb8b71a06f
decl5th <alsgur6624@gmail.com>
2024-07-29 11:09:48 +0900

    로그인 메시지 @bean 부여
    회원가입 양식 셋업

---
+++ src/main/java/org/choongang/global/member/controllers/RequestJoin.java
@@ -0,0 +1,30 @@
+package org.choongang.global.member.controllers;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+// 커맨드 객체
+@Data
+public class RequestJoin {
+
+    @NotBlank @Email
+    private String email;
+
+    @NotBlank @Size(min = 8)
+    private String password;
+
+    @NotBlank
+    private String confirmPassword;
+
+    @NotBlank
+    private String userName;
+
+    @NotBlank
+    private String mobile;
+
+    @AssertTrue // 필수항목으로 반드시 true가 되어야만 하는 애너테이션
+    private boolean agree;
+}
