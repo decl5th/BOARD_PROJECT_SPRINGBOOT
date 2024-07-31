1a121c007498ac3da06470f0f46d58afab103573
decl5th <alsgur6624@gmail.com>
2024-07-31 10:46:25 +0900

    로그인 검증 메시지 출력 기능 확인

--- src/main/java/org/choongang/member/services/LoginFailureHandler.java
+++ src/main/java/org/choongang/member/services/LoginFailureHandler.java
@@ -3,6 +3,8 @@ package org.choongang.member.services;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.choongang.member.controllers.RequestLogin;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
@@ -13,6 +15,15 @@ public class LoginFailureHandler implements AuthenticationFailureHandler {
     @Override
     // 로그인 실패시에 유입되는 메서드
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+
+        // 세션을 가져와서 모델로 -> 세션을 어케 가져올까?
+        HttpSession session = request.getSession();
+
+        RequestLogin form = new RequestLogin();
+        form.setEmail(request.getParameter("email"));
+        form.setPassword(request.getParameter("password"));
+
+        session.setAttribute("requestLogin", form);
 
         // request 와 response 있는 상태에서 페이지 이동 => response의 Redirect사용 = 로그인 실패시 로그인 페이지이동
         response.sendRedirect(request.getContextPath() + "/member/login");
