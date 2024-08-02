0df01f3fbc30499e84d84b5cb5075df353422753
decl5th <alsgur6624@gmail.com>
2024-08-02 12:01:12 +0900

    1.json data 입력 및 예외 응답 코드 처리 구현 및 테스트 2. 테스트 중 에러 발견 - csrf 토큰 추가

--- src/main/java/org/choongang/file/controllers/FileController.java
+++ src/main/java/org/choongang/file/controllers/FileController.java
@@ -1,22 +1,36 @@
 package org.choongang.file.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.choongang.file.entities.FileInfo;
+import org.choongang.file.services.FileUploadService;
+import org.choongang.global.exceptions.RestExceptionProcessor;
+import org.choongang.global.rests.JSONData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.UUID;
+import java.util.List;
 
 @RestController
 @RequestMapping("/file")
-public class FileController {
+@RequiredArgsConstructor
+public class FileController implements RestExceptionProcessor {
+    //  RestExceptionProcessor : json 형태 오류도 같이 출력
+
+    private final FileUploadService uploadService;
 
     @PostMapping("/upload")
-    public void upload(@RequestPart("file") MultipartFile[] files,
-                       @RequestParam(name = "gid", required = false ) String gid,
-                       @RequestParam(name = "location", required = false  ) String location) {
+    public ResponseEntity<JSONData> upload(@RequestPart("file") MultipartFile[] files,
+                                          @RequestParam(name = "gid", required = false ) String gid,
+                                          @RequestParam(name = "location", required = false  ) String location) {
 
+        List<FileInfo> items = uploadService.upload(files, gid, location);
+        HttpStatus status = HttpStatus.CREATED;
+        JSONData data = new JSONData(items);
+        data.setStatus(status);
+
+        return ResponseEntity.status(status).body(data);
     }
 
-    private String getGid() {
-        return UUID.randomUUID().toString();
-    }
 }
