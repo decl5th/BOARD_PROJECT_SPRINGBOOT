d50f5868ff385b418a991fc2b4f978ad75efe84a
decl5th <alsgur6624@gmail.com>
2024-08-02 11:01:40 +0900

    파일 정보 저장 - 엔티티 구성

--- src/main/java/org/choongang/file/entities/FileInfo.java
+++ src/main/java/org/choongang/file/entities/FileInfo.java
@@ -17,26 +17,23 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor @AllArgsConstructor
 public class FileInfo extends BaseMemberEntity {
-
     @Id @GeneratedValue
-    private long seq; // 서버에 업로드 될 파일 이름 - ex) seq.확장자
+    private Long seq; // 서버에 업로드될 파일 이름  - seq.확장자
 
-    @Column(length = 45, nullable = false)
+    @Column(length=45, nullable = false)
     private String gid = UUID.randomUUID().toString(); // 그룹 ID
 
-    @Column(length = 45)
-    private String name; // 파일 이름
-
-    @Column(length = 80, nullable = false)
+    @Column(length=45)
     private String location; // 그룹 안에 세부 위치
 
-    @Column(length = 80, nullable = false)
-    private String FileName;
+    @Column(length=80, nullable = false)
+    private String fileName;
 
-    @Column(length = 80)
+    @Column(length=30)
+    private String extension; // 파일 확장자
+
+    @Column(length=80)
     private String contentType;
 
     private boolean done; // 그룹 작업 완료 여부
-
-
 }
