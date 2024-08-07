9490a57d03e610c3b3705aca877c8785bb87b753
decl5th <alsgur6624@gmail.com>
2024-08-07 14:53:08 +0900

    파일 업로드 기능 마무리 지도 api 카카오 가져오기 시작

--- src/main/java/org/choongang/file/services/FileUploadService.java
+++ src/main/java/org/choongang/file/services/FileUploadService.java
@@ -22,6 +22,7 @@ public class FileUploadService {
 
     private final FileInfoRepository fileInfoRepository;
     private final FileProperties properties;
+    private final FileInfoService fileInfoService;
 
     public List<FileInfo> upload(MultipartFile[] files, String gid, String location) {
         /**
@@ -74,6 +75,8 @@ public class FileUploadService {
                 fileInfoRepository.flush();
             }
         }
+
+        uploadedFiles.forEach(fileInfoService::addFileInfo);
 
         return uploadedFiles;
     }
