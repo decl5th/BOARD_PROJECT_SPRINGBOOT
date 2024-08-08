a256be41045f097cb2e80be1a2c5f754f658bb78
decl5th <alsgur6624@gmail.com>
2024-08-08 20:15:12 +0900

    지도 api 세부 기능 구현 1. 인포윈도우, 클릭으로 인포윈도우 출력, 제거
    2. 키워드를 가지고 지도 검색
    3. 주소를 가지고 지도 검색
    4. 카테고리를 가지고 지도 검색

--- src/main/resources/static/common/js/map.js
+++ src/main/resources/static/common/js/map.js
@@ -9,12 +9,13 @@ const mapLib = {
      *                - center: {lat: 위도, lng: 경도, ... }  - 필수 항목
      *                - zoom: 확대 정도(1~10) / 숫자가 작을 수록 확대
      *                - markerImage : 공통 마커 이미지 주소값, 개별 마커 이미지가 있는 경우는 그걸로 대체,
-     *                - marker : [{lat: 위도, lng: 경도, info: html 데이터(인포윈도우 출력값), images: 이미지 경로 주소값 - 마커 이미지}, ... ] (배열 형태)
-     *
+     *                - marker : [{ lat: 위도, lng: 경도,
+     *                                 info: { content: html 데이터(인포윈도우), clickable: true|false - true(마커 클릭시 노출) }, image: 이미지 주소 - 마커이미지}]     *
      */
     load(mapId, width = 300, height = 300, options) { // 너비x높이 300px 기본값 지정
         const mapEl = document.getElementById(mapId);
         if (!mapEl || !options?.center ) return; // center 필수항목으로 설정
+
         mapEl.style.width = `${width}px`;
         mapEl.style.height = `${height}px`;
 
@@ -38,16 +39,41 @@ const mapLib = {
             if (!Array.isArray(marker)) marker = [marker]; // 배열 객체 아닐 때 즉, 일반 객체 일때
 
             const markers = marker.map(m => {
+                const {lat, lng, image, info} = m;
+                const opt = { position: new kakao.maps.LatLng(lat, lng)};
 
-                const opt = { position: new kakao.maps.LatLng(m.lat, m.lng)};
-
-                const mi = markerImage ? markerImage : m?.image; // marker 이미지 처리
+                const mi = markerImage ? markerImage : image; // marker 이미지 처리
                 if (mi) {
                     const mImage = new kakao.maps.MarkerImage(mi, new kakao.maps.Size(64, 69), {offset: new kakao.maps.Point(27, 69)});
                     opt.image = mImage;
                 }
-
                 const _marker = new kakao.maps.Marker(opt);
+
+                // 인포 윈도우 처리
+                if (info?.content) {
+                    const { content, clickable, removable } = info;
+
+                    const infoWindow = new kakao.maps.InfoWindow({
+                        content,
+                        removable: Boolean(removable),
+                    });
+
+                    if (clickable) { // 마커 클릭시 노출
+                        kakao.maps.event.addListener(_marker, "click", function() {
+                            if (_marker.isInfoWindowOpen) {
+                                infoWindow.close();
+                                _marker.isInfoWindowOpen = false;
+                            } else {
+                                infoWindow.open(map, _marker);
+                                _marker.isInfoWindowOpen = true;
+                            }
+                        });
+                    } else { // 바로 노출
+                        infoWindow.open(map, _marker);
+                    }
+                }
+
+
 
                 _marker.setMap(map);
 
@@ -55,8 +81,97 @@ const mapLib = {
             });
 
         } // endif
-        // 마커 출력 처리 D
+        // 마커 출력 처리 E
+    },
+    /**
+     * 현재 위치 기반으로 중심위치를 잡고 지도 출력
+     *
+     */
+    loadCurrentLocation(mapId, width = 300, height = 300, options) {
+        navigator.geolocation.getCurrentPosition(pos => {
+            const { latitude, longitude } = pos.coords;
+            options = options ?? {};
+            options.center = { lat: latitude, lng: longitude };
 
+            mapLib.load(mapId, width, height, options);
+        });
+    },
+    /**
+     * 키워드로 지도 출력
+     *
+     */
+    loadByKeyword(keyword, cnt = 0, mapId, width = 300, height = 300, options) {
+        if (!keyword?.trim()) return;
+
+        const ps = new kakao.maps.services.Places();
+
+        ps.keywordSearch(keyword.trim(), (items, status, pagination) => {
+            if (status === kakao.maps.services.Status.OK) { // 검색 성공
+                // cnt가 0이면 전체 목록, 1 이상이면 갯수 제한
+                items = cnt > 0 ? items.slice(0, cnt + 1) : items;
+
+                options = options ?? {};
+                options.center = { lat: items[0].y, lng: items[0].x };
+                options.marker = [];
+
+                items.forEach(item => {
+                    options.marker.push({lat: item.y, lng: item.x});
+                });
+            }
+            mapLib.load(mapId, width, height, options);
+        });
+    },
+    /**
+     * 주소로 지도 검색
+     */
+    loadByAddress(address, cnt = 0, mapId, width = 300, height = 300, options) {
+        if (!address?.trim()) return;
+
+        const geocoder = new kakao.maps.services.Geocoder();
+
+        geocoder.addressSearch(address, (items, status) => {
+            if (status === kakao.maps.services.Status.OK) { // 검색 성공
+                // cnt가 0이면 전체 목록, 1 이상이면 갯수 제한
+                items = cnt > 0 ? items.slice(0, cnt + 1) : items;
+
+                options = options ?? {};
+                options.center = { lat: items[0].y, lng: items[0].x };
+                options.marker = [];
+
+                items.forEach(item => {
+                    options.marker.push({lat: item.y, lng: item.x});
+                });
+            }
+
+            mapLib.load(mapId, width, height, options);
+        });
+    },
+    /**
+     * 카테고리별 주소 검색
+     */
+    loadByCategory(category, cnt = 0, mapId, width = 300, height = 300, options) {
+        if (!category?.trim()) return;
+
+        const ps = new kakao.maps.services.Places();
+
+        ps.categorySearch(category.trim(), placesSearchCB, {useMapBounds:true});
+
+        function placesSearchCB (items, status, pagination) {
+            console.log(pagination);
+            if (status === kakao.maps.services.Status.OK) {
+                // cnt가 0이면 전체 목록, 1 이상이면 갯수 제한
+                items = cnt > 0 ? items.slice(0, cnt + 1) : items;
+
+                options = options ?? {};
+                options.center = { lat: items[0].y, lng: items[0].x };
+                options.marker = [];
+
+                items.forEach(item => {
+                    options.marker.push({lat: item.y, lng: item.x});
+                });
+            }
+
+            mapLib.load(mapId, width, height, options);
+        }
     }
-
 };
--- src/main/resources/static/front/js/tour/view2.js
+++ src/main/resources/static/front/js/tour/view2.js
@@ -1,18 +1,26 @@
 window.addEventListener("DOMContentLoaded", function (){
-   const options = {
+
+    const html = "<h1>윈포윈도우 테스트</h1>";
+
+    const options = {
        center: {
            lat: 37.55772142387766,
            lng: 126.94321920274982,
        },
        marker: [
-           {lat:37.55772142387766, lng:126.94321920274982},
-           {lat:37.55648123246987, lng:126.94519221656536},
+           {lat:37.55772142387766, lng:126.94321920274982, info: { content: html, clickable: false, removable: true  }},
+           {lat:37.55648123246987, lng:126.94519221656536, info: { content: html, clickable: true, removable: false  }},
            {lat:37.55783517406359, lng:126.94560997964005},
        ],
        markerImage: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png",
    };
+    mapLib.loadByCategory("AT4", 0, "map1", 1000, 600, options);
 
-   mapLib.load("map1", 1000, 600, options);
+//    mapLib.loadByAddress("서울특별시 마포구 신촌로 176", 1, "map1", 1000, 600, options);
+    //mapLib.loadByKeyword("제주도 맛집",1, "map1", 1000, 600, options);
+    //mapLib.loadCurrentLocation("map1", 1000, 600, options);
+
+  // mapLib.load("map1", 1000, 600, options);
 
     /*
    mapLib.load("map1", 300, 300, options);
