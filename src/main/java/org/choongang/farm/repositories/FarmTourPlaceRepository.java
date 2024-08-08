a7af353f34573c2c6f34e486c1f419658e823e75
decl5th <alsgur6624@gmail.com>
2024-08-08 18:33:07 +0900

    3조 공공데이터 삭제

--- src/main/java/org/choongang/farm/repositories/FarmTourPlaceRepository.java
+++
@@ -1,8 +0,0 @@
-package org.choongang.farm.repositories;
-
-import org.choongang.farm.entities.FarmTourplace;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface FarmTourPlaceRepository extends JpaRepository<FarmTourplace, Long>, QuerydslPredicateExecutor<FarmTourplace> {
-}
